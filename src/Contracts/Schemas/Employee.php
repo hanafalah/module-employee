3058d9e817c5cd7c0219a120707383cd43efb200
hamzah <hamzahnuralfalah@gmail.com>
2025-03-29 04:16:47 +0700

    update employee module

--- src/Contracts/Schemas/Employee.php
+++ src/Contracts/Schemas/Employee.php
@@ -5,17 +5,25 @@ namespace Hanafalah\ModuleEmployee\Contracts\Schemas;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
+use Hanafalah\LaravelSupport\Data\PaginateData;
 use Hanafalah\ModuleEmployee\Data\EmployeeData;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 interface Employee extends DataManagement
 {
-    public function showUsingRelation(): array;
-    public function viewUsingRelation(): array;
     public function getEmployee(): mixed;
     public function prepareShowEmployee(?Model $model = null, ?array $attributes = null): Model;
     public function showEmployee(?Model $model = null): array;
+    public function prepareShowProfile(?Model $model = null, ?array $attributes = null): Model;
+    public function showProfile(?Model $model = null): array;
     public function prepareStoreEmployee(EmployeeData $employee_dto): Model;
     public function storeEmployee(? EmployeeData $employee_dto = null): array;
+    public function prepareViewEmployeePaginate(PaginateData $paginate_dto): LengthAwarePaginator;
+    public function viewEmployeePaginate(? PaginateData $paginate_dto = null): array;
+    public function prepareViewEmployeeList(): Collection;
+    public function viewEmployeeList(): array;
     public function employee(mixed $conditionals = null): Builder;
+    
     
 }
