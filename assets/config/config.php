572cb7d2b163d919f184dbf44619d5e34af68973
Hamzah <hamzahnafalah@gmail.com>
2025-05-03 12:43:04 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -7,22 +7,27 @@ use Hanafalah\ModuleEmployee\{
 };
 
 return [
+    'namespace' => 'Hanafalah\\ModuleEmployee',
     'app' => [
         'contracts' => [
-            // ADD YOUR CONTRACTS HERE
-        ],
-    ],
-    'commands' => [
-        ModuleEmployeeCommands\InstallMakeCommand::class
+            //ADD YOUR CONTRACTS HERE
+        ]
     ],
     'libs' => [
         'model' => 'Models',
         'contract' => 'Contracts',
-        'schema' => 'Schemas'
+        'schema' => 'Schemas',
+        'database' => 'Database',
+        'data' => 'Data',
+        'resource' => 'Resources',
+        'migration' => '../assets/database/migrations'
     ],
     'database' => [
         'models' => [
             // ADD YOUR MODELS HERE
-        ]
+            ]
+    ],
+    'commands' => [
+        ModuleEmployeeCommands\InstallMakeCommand::class
     ]
 ];
